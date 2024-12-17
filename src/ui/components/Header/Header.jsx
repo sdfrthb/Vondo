d8d1196c9e3249183f1fd3f91c44f5acd9c47a6b
estronnom <estronnom@gmail.com>
2024-12-17 09:55:57 +0300

    Dynamic header mvp

--- src/ui/components/Header/Header.jsx
+++ src/ui/components/Header/Header.jsx
@@ -1,60 +1,73 @@
-import { useEffect, useState } from "react";
-import { Desktop } from "../../../lib/media/request";
-import ScrollComponent from "../../../utils/ScrollComponent";
+import {useEffect, useMemo, useState} from "react";
+import {Desktop} from "../../../lib/media/request";
 import Logo from "../../icons/Logo/Logo";
 import HeaderLink from "../HeaderLink/HeaderLink";
 import styles from "./Header.module.css";
 
-function Header({ mainPage }) {
-  // const isDesktop = useMediaQuery({ minWidth: 1440 })
-  const [style, setStyle] = useState(0);
-  // console.log(style);
+const HEADER_HEIGHT = 80
+const INTRO_HEIGHT = 320
+const DYNAMIC_HEADER_HEIGHT = HEADER_HEIGHT + INTRO_HEIGHT
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = document.documentElement.scrollTop;
-      let translateValue = Math.max(0, scrollTop / 3);
-      mainPage && setStyle(translateValue);
-    };
+function Header({mainPage}) {
+    // const isDesktop = useMediaQuery({ minWidth: 1440 })
 
-     mainPage && window.addEventListener("scroll", handleScroll, { passive: true });
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+    // TODO:
+    // 1) Убрать отсюда весь мусор, все стили перенести из компонента в css (кроме тех, которые задаются по условию)
+    // 2) В компоненте Header оставить только <header>. Создать еще один компонент DynamicHeader, внутри которого будет рендерится Header + Intro (блок с текстом над видео).
+    // 3) В пропсы Header добавь возможность прокидывать нужные для работы стили, в данном случае это position, height, marginTop итд
 
-  return (
-    <header
-      style={
-        mainPage
-          ? {
-              height: `calc(11.528vw - ${style}px)`,
-              position: style > 17 ? "static" : "fixed",
-            }
-          : { position: "static", height: "auto" }
-      }
-      className={styles.header}
-    >
-      <Desktop></Desktop>
-      <nav className={styles.nav_menu}>
-        <HeaderLink link={"/cases"}>Кейсы</HeaderLink>
-        <HeaderLink link={"/principles"}>Принципы</HeaderLink>
-        <HeaderLink link={"/journal"}>Журнал</HeaderLink>
-        <HeaderLink link={"/education"}>Школа</HeaderLink>
-      </nav>
-      {mainPage ? (
-        <div className={styles.scroll}>
-          <Logo width={11.111} height={2.5} />
-        </div>
-      ) : (
-        <Logo width={6.25} height={1.389} />
-      )}
+    const [scrollTop, setScrollTop] = useState(document.documentElement.scrollTop)
+    const {isIntroVisible, logoOffset, logoScale} = useMemo(() => {
+        const logoOffset = Math.max(INTRO_HEIGHT - scrollTop - 30, 0)
 
-      <nav className={styles.nav_menu}>
-        <HeaderLink link={"/brif"}>Бриф</HeaderLink>
-        <HeaderLink link={"/contacts"}>Контакты</HeaderLink>
-        <HeaderLink link={"#form"}>Оценить проект</HeaderLink>
-      </nav>
-    </header>
-  );
+        return {
+            isIntroVisible: (scrollTop + HEADER_HEIGHT) < DYNAMIC_HEADER_HEIGHT,
+            logoScale: 1 + (logoOffset / INTRO_HEIGHT),
+            logoOffset: logoOffset,
+        }
+    }, [scrollTop])
+
+    useEffect(() => {
+        const handleScroll = () => setScrollTop(document.documentElement.scrollTop)
+        window.addEventListener("scroll", handleScroll, {passive: true});
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <>
+            <header
+                className={styles.header}
+                style={{
+                    height: `${HEADER_HEIGHT}px`,
+                    position: isIntroVisible ? 'fixed' : 'static',
+                    marginTop: isIntroVisible ? 0 : `${INTRO_HEIGHT}px`
+                }}
+            >
+                <Desktop></Desktop>
+                <nav className={styles.nav_menu}>
+                    <HeaderLink link={"/cases"}>Кейсы</HeaderLink>
+                    <HeaderLink link={"/principles"}>Принципы</HeaderLink>
+                    <HeaderLink link={"/journal"}>Журнал</HeaderLink>
+                    <HeaderLink link={"/education"}>Школа</HeaderLink>
+                </nav>
+                <div
+                    style={{transform: `translateY(+${logoOffset}px) scale(${logoScale})`}}>
+                    <Logo width={6.25} height={1.389}/>
+                </div>
+                <nav className={styles.nav_menu}>
+                    <HeaderLink link={"/brif"}>Бриф</HeaderLink>
+                    <HeaderLink link={"/contacts"}>Контакты</HeaderLink>
+                    <HeaderLink link={"#form"}>Оценить проект</HeaderLink>
+                </nav>
+            </header>
+            {isIntroVisible && <div
+                style={{
+                    width: '100%',
+                    height: `${DYNAMIC_HEADER_HEIGHT}px`,
+                }}
+            />}
+        </>
+    );
 }
 
 export default Header;
