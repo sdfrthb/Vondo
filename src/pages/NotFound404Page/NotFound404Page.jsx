d62822ab7bf5502741bdceb719874f32f5bbc8b9
Tanya Zakharova <sdfrthb23@yandex.ru>
2025-02-13 12:10:01 +0300

    new 404

--- src/pages/NotFound404Page/NotFound404Page.jsx
+++ src/pages/NotFound404Page/NotFound404Page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./NotFound404Page.module.css";
 import TextButton from "../../ui/components/TextButton/TextButton";
 import { useMediaQuery } from "react-responsive";
-
+import TextIconButton from "../../ui/components/TextIconButton/TextIconButton";
 
 function NotFound404Page() {
   const isDesktop = useMediaQuery({ minWidth: 1440 });
@@ -10,30 +10,31 @@ function NotFound404Page() {
   return (
     <div className={styles.page}>
       <div className={styles.wrapper}>
-        <h1 className={`text ${styles.error}`}>404</h1>
+        <div className={styles.titles}>
+          <h1 className={`text text_type_h1`}>404</h1>
+          <p className={`text text_type_accent_s`}>Что‑то не открылось</p>
+        </div>
         <div className={styles.contacts}>
-          <TextButton text={"+7 912 992-53-84"} url={"tel:89129925384"} type={"xs"} />
-          <TextButton text={"hello@vondo.ru"} url={"mailto:hello@vondo.ru"} type={"xs"}/>
+          <TextButton
+            text={"+7 929 236 27 70"}
+            url={"tel:89292362770"}
+            type={"s"}
+          />
+          <TextButton
+            text={"hello@vondo.ru"}
+            url={"mailto:hello@vondo.ru"}
+            type={"s"}
+          />
         </div>
       </div>
       <div className={styles.wrapper_btn}>
-        {isDesktop ? (
-          <p className={`text text_type_accent_s`}>
-            что-то
-            <br />
-            не открылось
-          </p>
-        ) : (
-          <p className={`text text_type_m`}>
-            что-то
-            <br />
-            не открылось
-          </p>
-        )}
-
-        <a className={`text text_type_m ${styles.link}`} href="/">
-          на главную
-        </a>
+        <TextIconButton
+          icon={"arrow up"}
+          redirect
+          text={"Вернуться на главную"}
+          url={"/"}
+          side={"left"}
+        />
       </div>
     </div>
   );
